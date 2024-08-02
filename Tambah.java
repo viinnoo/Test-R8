7703cfe268a34b8849664bc5423024b5b79c71bf
VINO <vinokazu_firmansyah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:05:52 +0700

    Add files via upload

---
+++ Tambah.java
@@ -0,0 +1,10 @@
+public class Tambah {
+    public static void main(String[] args) {
+       int a = 1;
+       int b = 200;
+       int hasil = a+b;
+       System.out.println("Hasil dari " + a + " ditambah " + b + " adalah " + hasil);
+
+    }
+    
+}
